e09847063f418dae348e7f333779d4a287bdbc96
Ilya Medvedev <imedvedev@readymag.com>
2019-01-15 00:00:18 +0300

    Add interactive mode

--- README.md
+++ README.md
@@ -1,10 +1,41 @@
 [![npm version](https://badge.fury.io/js/sls-tool.svg)](https://www.npmjs.com/package/sls-tool) ![](https://img.shields.io/github/license/iam-medvedev/sls-tool.svg)
 
 # sls
+
 A CLI tool that displays available scripts from package.json
 
-# Usage
-Run this command in your directory
+# Installation
+
+```
+$ yarn global add sls-tool
+or
+$ npm install -g sls-tool
+```
+
+# Static mode
+
+This mode just show list of scripts.
+
 ```
 $ sls
+
+package.json scripts:
+* start (react-scripts start)
+* build (react-scripts build)
+* test (react-scripts test --env=jsdom)
+* eject (react-scripts eject)
+```
+
+# Interactive mode
+
+This mode allows you select and run script from list.
+
+```
+$ sls -i
+
+package.json scripts:
+● start — react-scripts start
+○ build — react-scripts build
+○ test — react-scripts test --env=jsdom
+○ eject — react-scripts eject
 ```
--- index.js
+++ index.js
@@ -1,24 +1,81 @@
 #!/usr/bin/env node
 const path = require("path");
 const fs = require("fs");
+const cliSelect = require("cli-select");
+const { exec } = require("child_process");
 
 const currentDir = process.cwd();
 const packageJsonPath = path.resolve(currentDir, "package.json");
+const isInteractive = process.argv.includes("-i");
 
-if (fs.existsSync(packageJsonPath)) {
-  try {
-    const data = require(packageJsonPath);
-    if (data && data.scripts) {
-      console.log("package.json scripts:");
-      Object.keys(data.scripts).forEach(key => {
-        console.log(`* ${key} (${data.scripts[key]})`);
+/**
+ * Interactive mode
+ */
+const runInteractive = data => {
+  const env = Object.assign({}, process.env);
+  env.PATH =
+    path.resolve("./node_modules/.bin") +
+    (process.platform === "win32" ? ";" : ":") +
+    env.PATH;
+
+  const options = {
+    cleanup: true,
+    selected: "●",
+    unselected: "○",
+    values: Object.keys(data.scripts).map(key => ({
+      command: data.scripts[key],
+      name: key
+    })),
+    valueRenderer: (item, selected) => {
+      if (selected) {
+        return "\033[4m" + item.name + "\033[0m" + " — " + item.command;
+      }
+
+      return `${item.name} — ${item.command}`;
+    }
+  };
+
+  cliSelect(options, result => {
+    if (result.id !== null) {
+      const child = exec(result.value.command, {
+        cwd: process.cwd(),
+        env: env
+      });
+
+      child.stdout.on("data", function(data) {
+        console.log(data.toString());
       });
     } else {
-      console.error("package.json does not contains any scripts 🤔");
+      console.log("Bye");
     }
-  } catch (err) {
-    console.error("There is something wrong with your package.json 🤔");
+  });
+};
+
+/**
+ * Static mode
+ */
+const runStatic = data => {
+  Object.keys(data.scripts).forEach(key => {
+    console.log(`* ${key} (${data.scripts[key]})`);
+  });
+};
+
+const run = () => {
+  if (fs.existsSync(packageJsonPath)) {
+    try {
+      const data = require(packageJsonPath);
+      if (data && data.scripts) {
+        console.log("package.json scripts:");
+        isInteractive ? runInteractive(data) : runStatic(data);
+      } else {
+        console.error("package.json does not contains any scripts 🤔");
+      }
+    } catch (err) {
+      console.error("There is something wrong with your package.json 🤔");
+    }
+  } else {
+    console.error("package.json is not exists in current folder 😞");
   }
-} else {
-  console.error("package.json is not exists in current folder 😞");
-}
+};
+
+run();
